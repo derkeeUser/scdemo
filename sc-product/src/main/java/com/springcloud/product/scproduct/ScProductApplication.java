4fba8c9e58db0ab371e956a17880da148848782c
陈晓明 <18594278531@163.com>
2020-07-30 16:10:24 +0800

    product-server集成服务监控(promethus,grafana)与告警(grafana)服务

--- sc-product/src/main/java/com/springcloud/product/scproduct/ScProductApplication.java
+++ sc-product/src/main/java/com/springcloud/product/scproduct/ScProductApplication.java
@@ -1,9 +1,13 @@
 package com.springcloud.product.scproduct;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * <功能简述> 
@@ -21,5 +25,11 @@ public class ScProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScProductApplication.class, args);
         System.out.println("服务提供方启动成功 √");
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 }
